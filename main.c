d1d9acba91c2eb33d09830898ef6e6f8614433be
Leif Johansson <leifj@sunet.se>
2016-10-24 23:15:16 +0200

    debug, docs, cleanup etc

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <assert.h>
+#include <stdarg.h>
  
 #define CHECK(f, rv) \
     if (SCARD_S_SUCCESS != rv) \
@@ -15,16 +16,48 @@
         fprintf(stderr, f ": %s\n", pcsc_stringify_error(rv)); \
         return -1; \
     }
-#define CHECK_RESPONSE(buffer, bufferLength) \
-    if(buffer[bufferLength-2] != 0x90 || buffer[bufferLength-1] != 0x00) { \
-        fprintf(stderr, "Invalid response!\n"); \
+#define CHECK_RESPONSE(buffer, bufferLength, okCode) \
+    if (verbose) { fprintf(stderr,"Response: %x:%x\n", buffer[bufferLength-2], buffer[bufferLength-1]); } \
+    if (buffer[bufferLength-2] != 0x90 || buffer[bufferLength-1] != okCode) { \
+        fprintf(stderr, "invalid response from card\n"); \
         return -2; \
     }
+
+void err(const int code, const char *msg, ...) {
+   va_list ap;
+
+   va_start(ap, msg);
+   vfprintf(stderr, msg, ap);
+   va_end(ap);
+   exit(code);
+}
 
 typedef enum mode_enum { READ, WRITE, PROTECT } rwp_t;
 
 extern char *optarg;
 extern int optind, opterr, optopt;
+
+void usage(int ec) {
+   printf("usage: mcrw \n");
+   printf("     [-R <n>]            : use PCSC reader <n>\n");
+   printf("     [-r]                : read <length> bytes from card at <offset>\n");
+   printf("     [-w]                : write <length> bytes (or until EOF) to card at <offset>\n");
+   printf("     [-o <offset (0)> ]  : offset of read/write operation - must be < 255\n");
+   printf("     [-l <length> ]      : number of bytes to read/write\n");
+   printf("     [-B]                : when wreading output raw bytes to <file>\n");
+   printf("     [-f <file (-)>]     : read data from / write data to this file.\n");
+   printf("     [-C <code (ffffff)>]: use this hex code as secret code for writing\n");
+   printf("     [-v]                : a bit more output\n");
+   printf("     [-h]                : print this message\n");
+   printf("\n");
+   printf("If multiple card readers are present and -R is not provided a list of the\n");
+   printf("available readers will be printed on stdout and the program will exit with\n");
+   printf("a non-zero exit code. Re-run the program with the apropriate argument to -R\n");
+   printf("\n");
+   printf("If '-' is given as the filename, stdin/stdout will be used.\n");
+   printf("\n");
+   exit(ec);
+}
 
 int main(int argc, char *argv[]) {
     LONG rv;
@@ -35,8 +68,10 @@ int main(int argc, char *argv[]) {
     SCARD_IO_REQUEST pioSendPci;
     BYTE pbRecvBuffer[266];
     BYTE pbSendBuffer[266];
-    unsigned int i;
-    unsigned int length = 128;
+    unsigned int i, sz, remaining;
+    unsigned int length = 256;
+    unsigned int nbytes = 0;
+    unsigned int writeCode = 0xffffff;
     BYTE offset = 0;
     rwp_t mode = READ;
     unsigned int msglen = 0;
@@ -44,6 +79,8 @@ int main(int argc, char *argv[]) {
     char *ptr, **readers = NULL;
     int fd,nbReaders;
     int rid = -1;
+    int binary_output = 0;
+    int verbose = 0;
  
     BYTE cmdDefineCardType[] = { 0xFF, 0xA4, 0x00, 0x00, 0x01, 0x06 };
     BYTE cmdWriteCard[] = { 0xFF, 0xD0, 0x00 };
@@ -52,8 +89,14 @@ int main(int argc, char *argv[]) {
     memset(pbSendBuffer,0,265);
     memset(&pioSendPci,0,sizeof(SCARD_IO_REQUEST));
 
-    while ((i = getopt(argc, argv, "rwo:l:R:")) != EOF) {
+    while ((i = getopt(argc, argv, "vhrwo:l:R:BC:")) != EOF) {
        switch (i) {
+       case 'v':
+          verbose = 1;
+          break;
+       case 'h':
+          usage(0);
+          break;
        case 'r':
           mode = READ;
           break;
@@ -66,19 +109,28 @@ int main(int argc, char *argv[]) {
        case 'R':
           rid = strtol(optarg, NULL, 10);
           break;
+       case 'B':
+          binary_output = 1;
+          break;
        case 'o':
           offset = strtol(optarg, NULL, 16);
           if (offset > 256) {
-             fprintf(stderr, "Invalid argument to -o. Must be < 256.\n");
-             exit(3); 
+             err(3, "Invalid argument to -o. Must be < 256.\n");
           }
           break;
        case 'l':
-          length = strtol(argv[2], NULL, 16);
+          length = strtol(optarg, NULL, 16);
           if (length > 256) {
-             fprintf(stderr, "Invalid argument to -l. Must be < 256.\n");
-             exit(3);
+             err(3, "Invalid argument to -l. Must be < 256.\n");
           }
+          break;
+       case 'C':
+          writeCode = strtol(optarg, NULL, 16);
+          if (writeCode > 0xffffff) {
+             err(3, "Invalid argument to -C. Must be < 0xffffff.\n");
+          }
+       default:
+          usage(3);
           break;
        }
     }
@@ -100,15 +152,13 @@ int main(int argc, char *argv[]) {
     }
 
     if (nbReaders == 0) {
-       printf("No reader found\n");
-       exit(-1);
+       err(1, "no reader found\n");
     }
 
     /* allocate the readers table */
     readers = calloc(nbReaders, sizeof(char *));
     if (NULL == readers) {
-       printf("out of memory... bye!\n");
-       exit(1);
+       err(1, "out of memory... bye!\n");
     }
 
     /* fill the readers table */
@@ -123,9 +173,9 @@ int main(int argc, char *argv[]) {
     if (nbReaders == 1) {
        rid = 0;
     } else if (rid == -1) {
-       printf("Available readers (use -R to select)\n");
+       fprintf(stdout, "Available readers (use -R to select)\n");
        for (int i = 0; i < nbReaders; i++) {
-          printf("%d: %s\n", i, readers[i]);
+          fprintf(stdout, "%d: %s\n", i, readers[i]);
        }
        printf("\n");
        exit(0);
@@ -162,7 +212,7 @@ int main(int argc, char *argv[]) {
  
     rv = SCardTransmit(hCard, &pioSendPci, cmdDefineCardType, sizeof(cmdDefineCardType), NULL, pbRecvBuffer, &dwRecvLength);
     CHECK("SCardTransmit (define card type)", rv)
-    CHECK_RESPONSE(pbRecvBuffer, dwRecvLength);
+    CHECK_RESPONSE(pbRecvBuffer, dwRecvLength, 0x00);
 
     if (mode == READ) {
        pbSendBuffer[0] = 0xFF;
@@ -177,30 +227,75 @@ int main(int argc, char *argv[]) {
        } else {
           fd = open(file_name,O_CREAT|O_WRONLY);
           if (fd == -1) {
-             fprintf(stderr, "error opening %s for writing: %s\n", file_name, strerror(errno));
-             exit(2);
+             err(1, "error opening %s for writing: %s\n", file_name, strerror(errno));
           }
        }
     } else if (mode == WRITE) {
+       /* unlock card for writing */
+       pbSendBuffer[0] = 0xFF;
+       pbSendBuffer[1] = 0x20;
+       pbSendBuffer[2] = 0x00;
+       pbSendBuffer[3] = 0x00;
+       pbSendBuffer[4] = 0x03;
+       msglen = 8;
+       memcpy(&pbSendBuffer[5],&writeCode,3);
+       dwRecvLength = sizeof(pbRecvBuffer);
+       rv = SCardTransmit(hCard, &pioSendPci, pbSendBuffer, msglen, NULL, pbRecvBuffer, &dwRecvLength);
+       CHECK("SCardTransmit (unlock)", rv);
+       CHECK_RESPONSE(pbRecvBuffer, dwRecvLength, 0x07);
+
+       memset(pbSendBuffer,0,265);
+       memset(pbRecvBuffer,0,265);
        pbSendBuffer[0] = 0xFF;
        pbSendBuffer[1] = 0xD0;
        pbSendBuffer[2] = 0x00;
        pbSendBuffer[3] = offset;
-       pbSendBuffer[4] = length;
-       msglen = 5+length;
+       msglen = 5;
+  
+       if (strncmp(file_name,"-",1) == 0) {
+          fd = STDIN_FILENO;
+       } else {
+          fd = open(file_name,O_RDONLY);
+          if (fd == -1) {
+             err(1, "error opening %s for writing: %s\n", file_name, strerror(errno));
+          }
+       }
+
+       ptr = &pbSendBuffer[5];
+       sz = 1;
+       while (length > 0 && sz > 0) {
+          sz = read(fd, ptr, length);
+          if (verbose)
+             fprintf(stderr, "read %d/%d bytes\n", sz, length);
+          length -= sz;
+          nbytes += sz;
+          ptr += sz;
+       }
+       if (sz == -1) {
+          err(2, "error reading data from %s: %s\n", file_name, strerror(errno));
+       }
+       pbSendBuffer[4] = nbytes;
+       msglen += nbytes;
+       if (verbose)
+          fprintf(stderr, "%x:%x:%x:%x:%x:'%s'\n",pbSendBuffer[0],pbSendBuffer[1],pbSendBuffer[2],pbSendBuffer[3],pbSendBuffer[4],&pbSendBuffer[5]);
     }
 
-    if (mode == WRITE) {
-       read(fd, &pbSendBuffer[5], length);
-    }
- 
     dwRecvLength = sizeof(pbRecvBuffer);
     rv = SCardTransmit(hCard, &pioSendPci, pbSendBuffer, msglen, NULL, pbRecvBuffer, &dwRecvLength);
     CHECK("SCardTransmit", rv)
-    CHECK_RESPONSE(pbRecvBuffer, dwRecvLength);
+    CHECK_RESPONSE(pbRecvBuffer, dwRecvLength, 0x00);
 
-    if (mode == READ) { 
-       write(fd, pbRecvBuffer, dwRecvLength-2);
+    if (verbose)
+       fprintf(stderr, "received %lu bytes\n", dwRecvLength);
+
+    if (mode == READ) {
+       if (binary_output) {
+          write(fd, pbRecvBuffer, dwRecvLength-2);
+       } else {
+          FILE *out = fdopen(fd, "w");
+          pbRecvBuffer[dwRecvLength-2] = '\0';
+          fprintf(out, "%s\n", &pbRecvBuffer[0]);
+       }
     }
  
     rv = SCardDisconnect(hCard, SCARD_LEAVE_CARD);
